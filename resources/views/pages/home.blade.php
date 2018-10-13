48bcfb680ed190f9e6e1f23c2151f9101bc0a85b
Christian Aldover <cka6@njit.edu>
2018-10-13 09:47:40 -0700

    changed h1 of home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.default')
 @section('content')
-    <h1>I am the Home Page</h1>
+    <h1>Home Page</h1>
     <h3>This is my assignment submission</h3>
     <h3>Please check out the About page too!</h3>
 @stop
