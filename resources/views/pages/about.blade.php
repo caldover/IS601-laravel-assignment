d7d8afce364c01d9fbbc4fc920475c8f4874739d
Christian Aldover <cka6@njit.edu>
2018-10-13 09:47:59 -0700

    changed h1 of about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.default')
 @section('content')
-    <h1>I am the About Page</h1>
+    <h1>About Page</h1>
     <h3>Student Name: Christian</h3>
     <h3>UCID: cka6</h3>
     <h3>City: Los Angeles</h3>
