6053fade87d5fc4ba7a02b569b02edc8db4475d7
Christian Aldover <cka6@njit.edu>
2018-10-13 09:35:07 -0700

    added base views files and started working on about page

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,1 @@
+<div id="copyright text-right">Footer goes here</div>
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('content')
+    <h1>I am the About Page</h1>
+    <h2>Student Name: Christian</h2>
+    <h2>UCID: cka6</h2>
+@stop
